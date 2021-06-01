c92d7451a5c052acc7919f37d7ad16448478ce1c
Claudio Collao <clcollao@alumnos.uai.cl>
2021-05-31 23:13:52 -0400

    Tercer commit

--- enron_analysis_data/enron_analysis_data/settings.py
+++ enron_analysis_data/enron_analysis_data/settings.py
@@ -1,9 +1,4 @@
 from pathlib import Path
 
-PACKAGE_PATH = Path(__file__).parent
-
-PROJECT_PATH = PACKAGE_PATH.parent
-
-CLEAN_DATA = Path(PACKAGE_PATH, "data/clean_data")
-
-RAW_DATA = Path(PACKAGE_PATH, "data/raw_data")
+CLEAN_DATA = Path('/Users/claudiocollaobahamondes/Documents/GitHub/enron_analysis_data/enron_analysis_data/data/clean_data')
+RAW_DATA = Path('/Users/claudiocollaobahamondes/Documents/GitHub/enron_analysis_data/enron_analysis_data/data/raw_data')
